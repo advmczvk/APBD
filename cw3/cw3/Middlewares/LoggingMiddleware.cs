280f94d4c186168a6bb5e6e8c01695fafa345140
advmczvk <kuba.adamczyk99@gmail.com>
2020-04-07 23:44:13 +0200

    Add cw6

---
+++ cw3/cw3/Middlewares/LoggingMiddleware.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cw3.Middlewares
+{
+    public class LoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public LoggingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            var httpMethod = httpContext.Request.Method;
+            var httpPath = httpContext.Request.Path;
+            var queryString = httpContext.Request.QueryString;
+            var body = "";
+            using (var sr = new StreamReader(httpContext.Request.Body, Encoding.UTF8, true, 1024))
+            {
+                body += await sr.ReadToEndAsync();
+            }
+
+            using (var sw = File.AppendText("requestsLog.txt"))
+            {
+                sw.WriteLine($"Method: {httpMethod}");
+                sw.WriteLine($"Path: {httpPath}");
+                sw.WriteLine($"Body:\n{body}");
+                sw.WriteLine($"QueryString: {queryString}\n");
+            }
+
+            await _next(httpContext);
+        }
+    }
+}
--- cw3/cw3/Startup.cs
+++ cw3/cw3/Startup.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using cw3.DAL;
+using cw3.Middlewares;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -30,8 +32,30 @@ namespace cw3
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IDbService dbService)
         {
+            app.UseMiddleware<LoggingMiddleware>();
+            
+            app.Use(async (context, next) => 
+            {
+                if (!context.Request.Headers.ContainsKey("Index"))
+                {
+                    context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                    await context.Response.WriteAsync("Index required");
+                }
+                else
+                {
+                    var index = context.Request.Headers["Index"].ToString();
+                    if(dbService.GetStudent(index) == null)
+                    {
+                        context.Response.StatusCode = StatusCodes.Status404NotFound;
+                        await context.Response.WriteAsync("Index doesn't exist");
+                    }
+                }
+
+                await next(); 
+            });
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
